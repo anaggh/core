8bf3f71b3252d390a2e8f088dcf0a12e5f5ab575
Anaggh Sreenath <anagghscm@gmail.com>
2016-08-24 19:57:31 +0530

    Add Social Media Link Support

--- php/footer.php
+++ php/footer.php
@@ -1,8 +1,24 @@
 <div class="footer">
 	<?php echo $Site->footer() ?> | Powered by <a href="https://www.bludit.com/" target="_blank">BLUDIT</a> | Theme: <a href="https://github.com/anaggh/core" target="_blank">Core</a> |
 		
-	<!-- Sitemap and RSS -->
+	<!-- Social Media Links, Sitemap and RSS -->
 	<?php
+		if($Site->twitter()) {
+			echo '<a href="'.$Site->twitter().'"><span> Twitter</span></a> |';
+		}
+
+		if($Site->facebook()) {
+			echo '<a href="'.$Site->facebook().'"><span> Facebook</span></a> |';
+		}
+
+		if($Site->instagram()) {
+			echo '<a href="'.$Site->instagram().'"><span> Instagram</span></a> |';
+		}
+
+		if($Site->github()) {
+			echo '<a href="'.$Site->github().'"><span> Github</span></a> |';
+		}
+
 		if( $plugins['all']['pluginRSS']->installed() ) {
 			echo '<a href="'.DOMAIN_BASE.'rss.xml'.'"><span> RSS</span></a> |';
 		}
@@ -10,7 +26,7 @@
 		if( $plugins['all']['pluginSitemap']->installed() ) {
 			echo '<a href="'.DOMAIN_BASE.'sitemap.xml'.'"><span> Sitemap</span></a> |';
 		}
-	?>	
+		?>	
 		
 	<!-- Go Top! Link -->
 	<a href="#">Go Top!</a> |		
