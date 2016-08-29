810255947e9d5541d9e609eeba689902b5c47494
Anaggh Sreenath <anagghscm@gmail.com>
2016-08-29 11:44:38 +0530

    Improve search

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 		<!-- Custom Search -->
 		<form role="search" action="https://encrypted.google.com/search">
 		<div class="form-group">
-			<input type="hidden" name="as_sitesearch" value="<?php echo $Site->url() ?>">
+			<input type="hidden" name="as_sitesearch" value="<?php echo preg_replace('/(?:https?:\/\/)?(?:www\.)?(.*)\/?$/i', '$1', $Site->url()); /* http://stackoverflow.com/questions/6738752/regex-for-dropping-http-and-www-from-urls */?>">
 			<input type="hidden" name="as_qdr" value="all">
 			<input type="text" name="as_q" class="form-control" placeholder="Search...">
 		</div>
