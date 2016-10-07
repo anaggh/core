6f62eaf6741e36384a737d4a4209321fbb833a8f
Anaggh Sreenath <anagghscm@gmail.com>
2016-10-07 22:08:42 +0530

    Use my search function instead of Google Search
    Use https://github.com/anaggh/Bludit-Search instead of Google Search.

--- index.php
+++ index.php
@@ -36,14 +36,23 @@
 		<!-- Sidebar -->
 		<div class="col sidebar">
 		
-		<!-- Custom Search -->
+		<!-- Custom Google Search | Disabled in theme for now. Uncomment if required. Also uncomment the php statement in hidden input.
 		<form role="search" action="https://encrypted.google.com/search">
 		<div class="form-group">
-			<input type="hidden" name="as_sitesearch" value="<?php echo preg_replace('/(?:https?:\/\/)?(?:www\.)?(.*)\/?$/i', '$1', $Site->url()); /* http://stackoverflow.com/questions/6738752/regex-for-dropping-http-and-www-from-urls */?>">
+			<input type="hidden" name="as_sitesearch" value="<?php// echo preg_replace('/(?:https?:\/\/)?(?:www\.)?(.*)\/?$/i', '$1', $Site->url()); /* http://stackoverflow.com/questions/6738752/regex-for-dropping-http-and-www-from-urls */?>">
 			<input type="hidden" name="as_qdr" value="all">
 			<input type="text" name="as_q" class="form-control" placeholder="Search...">
 		</div>
+		</form> -->
+		
+		<!-- Search -->
+		<form role="search" action="<?php echo $Site->url() . 'search.php';?>">
+		<div class="form-group">
+			<input type="hidden" name="filter" value="all">
+			<input type="text" name="searchQuery" class="form-control" placeholder="Search...">
+		</div>
 		</form>
+		 
 			
 		<!-- Load sidebar -->
 		<?php Theme::plugins('siteSidebar') ?>		
