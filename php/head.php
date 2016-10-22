f2956dbe31b992cc3e63b5a839f5c0c5e3ba3e4d
Anaggh Sreenath <anagghscm@gmail.com>
2016-10-22 12:36:22 +0530

    Disable referrer

--- php/head.php
+++ php/head.php
@@ -14,6 +14,8 @@
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.6.0/styles/default.min.css">
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.6.0/highlight.min.js"></script>
 <script>hljs.initHighlightingOnLoad();</script> 
+<!-- Disable referrer -->
+<meta name="referrer" content="no-referrer" />
 
 <?php
 
